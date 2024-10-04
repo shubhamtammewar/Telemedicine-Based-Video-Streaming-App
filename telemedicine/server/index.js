700384e4066921a326d139bda33993491f277f2a
shubhamtammewar <91666990+shubhamtammewar@users.noreply.github.com>
2024-10-05 00:06:16 +0530

    Repo set Up

---
+++ telemedicine/server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  socket.on('join', (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit('user-connected', socket.id);
+  });
+
+  socket.on('offer', (offer, roomId) => {
+    socket.to(roomId).emit('offer', offer);
+  });
+
+  socket.on('answer', (answer, roomId) => {
+    socket.to(roomId).emit('answer', answer);
+  });
+
+  socket.on('ice-candidate', (candidate, roomId) => {
+    socket.to(roomId).emit('ice-candidate', candidate);
+  });
+
+  socket.on('disconnect', () => {
+    // Handle disconnect
+  });
+});
+
+server.listen(3001, () => console.log('Server running on port 3001'));
